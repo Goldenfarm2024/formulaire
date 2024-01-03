f42856b11370fa3bcebda547f70b1960201f66ae
Cloud.goldenfarm2024 <155451124+Goldenfarm2024@users.noreply.github.com>
2024-01-02 21:08:58 -0900

    Update formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -1,6 +1,6 @@
 <?php
 
- if (isset($_POST['submit'])) {
+ if (isset($_GET['submit'])) {
 
     // Vérifiez que les données du formulaire sont valides
     $username = htmlspecialchars($_POST['username']);
